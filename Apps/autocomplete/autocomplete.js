dd3eed36b4104561df1794ef80ecabb9d5fbf2a5
Alvet <contact@alvet.com.au>
2017-06-20 11:12:01 +1000

    autocomplete update
    autocomplete update

--- Apps/autocomplete/autocomplete.js
+++ Apps/autocomplete/autocomplete.js
@@ -1,6 +1,6 @@
     var script = document.createElement('script');
     script.setAttribute('class', 'edsautocomplete-app');
-    script.setAttribute('data-a', 'https://widgets.ebscohost.com/prod/encryptedkey/eds/eds.php?k=eyJjdCI6ImpXeWF6K1BFWlc2QWlIMGVobGpwRWQzRUg2T0N4eFNxUUUxcVFVb0x2Tzg9IiwiaXYiOiI2ZmRiMDliN2I5MzMwMDMwZjMzYTBiZTU2NDkxYmVlMyIsInMiOiJlNjJiOWYxNTJiZjdhN2ZmIn0=&p=YWx2ZXRtLm1haW4uZWRzYXBp&s=0,1,1,0,0,0&q=');
+    script.setAttribute('data-a', '');
     script.setAttribute('data-q', 'search?query-1=AND,{searchterm}*&view=title&resultsperpage=5');
     script.setAttribute('data-s', '%23translControl1,%23searchsubmit,3,3,0,,0');
     script.type = 'text/javascript';
