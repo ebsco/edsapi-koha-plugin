c739e6f3e63ea7a6c18b97d73aa8d458956458f0
Cam Wu <cwu@ebsco.com>
2019-04-09 08:43:29 +1000

    Rebuild plugin

--- Koha/Plugin/EDS.pm
+++ Koha/Plugin/EDS.pm
@@ -25,7 +25,7 @@ our $MAJOR_VERSION = "17.11";
 our $SUB_VERSION = "006";
 our $VERSION = $MAJOR_VERSION . "" . $SUB_VERSION;
 our $SHA_ADD = "https://widgets.ebscohost.com/prod/api/koha/sha/1711.json";
-our $DATE_UPDATE = '2019-02-20';
+our $DATE_UPDATE = '2019-04-09';
 ######################################################
 
 ## Here is our metadata, some keys are required, some are optional
