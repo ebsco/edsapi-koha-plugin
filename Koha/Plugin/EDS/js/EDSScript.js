01a886bdbf9c6f001eb7706e568a9913a24cf8fb
Michael Abrahamson <mabrahamson@ebsco.com>
2020-06-12 15:03:21 -0500

    Adding new KPZ file for testing.

--- Koha/Plugin/EDS/js/EDSScript.js
+++ Koha/Plugin/EDS/js/EDSScript.js
@@ -30,7 +30,7 @@ var bibListLocal = 0;
 var searchBlockCount = 3;
 
 // DO NOT TOUCH - controlled by build.py
-var versionEDSKoha = "19.05003";
+var versionEDSKoha = "19.05004";
 ///////////////////////////////////////
 
 if (document.title == "") {
