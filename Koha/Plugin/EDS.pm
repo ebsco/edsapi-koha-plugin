6f2dd6c9078a0741d5cdd6a15813c2697ed082a7
Michael Abrahamson <mabrahamson@ebsco.com>
2020-07-13 10:18:21 -0500

    Corrects issues with cart message. Modified templates to handle functionality previously handled by eds_addRecord JS script.

--- Koha/Plugin/EDS.pm
+++ Koha/Plugin/EDS.pm
@@ -22,10 +22,10 @@ $PluginDir = $PluginDir.'/Koha/Plugin/EDS';
 
 ################# DO NOT TOUCH - CONTROLLED BY build.py
 our $MAJOR_VERSION = "19.05";
-our $SUB_VERSION = "005";
+our $SUB_VERSION = "006";
 our $VERSION = $MAJOR_VERSION . "" . $SUB_VERSION;
 our $SHA_ADD = "https://widgets.ebscohost.com/prod/api/koha/sha/1711.json";
-our $DATE_UPDATE = '2020-06-26';
+our $DATE_UPDATE = '2020-07-10';
 ######################################################
 
 ## Here is our metadata, some keys are required, some are optional
