5a4617e8fedca8503a2c1133bbae281ad85c4d65
CORP\mabrahamson <mabrahamson@ebsco.com>
2021-04-26 14:43:39 -0500

    Initial updates

--- Koha/Plugin/EDS/js/EDSScript.js
+++ Koha/Plugin/EDS/js/EDSScript.js
@@ -30,7 +30,7 @@ var bibListLocal = 0;
 var searchBlockCount = 3;
 
 // DO NOT TOUCH - controlled by build.py
-var versionEDSKoha = "20.05001";
+var versionEDSKoha = "19.11001";
 ///////////////////////////////////////
 
 if (document.title == "") {
