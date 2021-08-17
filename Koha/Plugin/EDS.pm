e30697b1800247c519afdeb91becfcb9f70a1a11
CORP\mabrahamson <mabrahamson@ebsco.com>
2021-08-17 11:07:01 -0500

    Corrects issue with default search not getting set

--- Koha/Plugin/EDS.pm
+++ Koha/Plugin/EDS.pm
@@ -33,7 +33,7 @@ our $MAJOR_VERSION = "21.05";
 our $SUB_VERSION = "001";
 our $VERSION = $MAJOR_VERSION . "" . $SUB_VERSION;
 our $SHA_ADD = "https://widgets.ebscohost.com/prod/api/koha/sha/1711.json";
-our $DATE_UPDATE = '2021-08-16';
+our $DATE_UPDATE = '2021-08-17';
 ######################################################
 
 ## Here is our metadata, some keys are required, some are optional
