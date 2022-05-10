0dc5d8d847c109d30cad7c12bbf376e9fdf40060
Michael Abrahamson <mabrahamson@ebsco.com>
2022-05-10 10:46:22 -0500

    Renaming all $ to jQuery.  Adding additional logging options.  Replacing jquery.cookie with built in Cookies.

--- Koha/Plugin/EDS.pm
+++ Koha/Plugin/EDS.pm
@@ -34,7 +34,7 @@ our $MAJOR_VERSION = "21.11";
 our $SUB_VERSION = "001";
 our $VERSION = $MAJOR_VERSION . "" . $SUB_VERSION;
 our $SHA_ADD = "https://widgets.ebscohost.com/prod/api/koha/sha/1711.json";
-our $DATE_UPDATE = '2022-05-09';
+our $DATE_UPDATE = '2022-05-10';
 ######################################################
 
 ## Here is our metadata, some keys are required, some are optional
