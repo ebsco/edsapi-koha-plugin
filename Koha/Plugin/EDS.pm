97bf471d7efc55b51daa4c3db65544c90bf457b0
Michael Mohkamkar <mmohkamkar@ebsco.com>
2023-08-02 17:01:34 -0400

    Deletes additional console logs and changes key name

--- Koha/Plugin/EDS.pm
+++ Koha/Plugin/EDS.pm
@@ -34,7 +34,7 @@ our $MAJOR_VERSION = "22.11";
 our $SUB_VERSION = "001";
 our $VERSION = $MAJOR_VERSION . "" . $SUB_VERSION;
 our $SHA_ADD = "https://widgets.ebscohost.com/prod/api/koha/sha/1711.json";
-our $DATE_UPDATE = '2023-07-31';
+our $DATE_UPDATE = '2023-08-02';
 ######################################################
 
 ## Here is our metadata, some keys are required, some are optional
