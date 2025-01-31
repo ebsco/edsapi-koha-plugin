19eaf464adc7afc626dc92b551c914e5b57e3e81
omuzzyEBSCO <omuzzy@ebsco.com>
2025-01-31 13:23:28 -0800

    first build of 24.05

--- Koha/Plugin/EDS.pm
+++ Koha/Plugin/EDS.pm
@@ -30,11 +30,11 @@ my ($PluginDir) = grep { -f $_ . "/Koha/Plugin/EDS.pm" } @pluginsdir;
 $PluginDir = $PluginDir.'/Koha/Plugin/EDS';
 
 ################# DO NOT TOUCH - CONTROLLED BY build.py
-our $MAJOR_VERSION = "23.11";
-our $SUB_VERSION = "002";
+our $MAJOR_VERSION = "24.05";
+our $SUB_VERSION = "001";
 our $VERSION = $MAJOR_VERSION . "" . $SUB_VERSION;
 our $SHA_ADD = "https://widgets.ebscohost.com/prod/api/koha/sha/1711.json";
-our $DATE_UPDATE = '2025-01-06';
+our $DATE_UPDATE = '2025-01-31';
 ######################################################
 
 ## Here is our metadata, some keys are required, some are optional
