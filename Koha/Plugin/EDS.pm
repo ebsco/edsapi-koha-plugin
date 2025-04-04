2f52b9223a2a2dc23102c3b89aef482ccf3e3553
omuzzyEBSCO <omuzzy@ebsco.com>
2025-04-04 10:49:48 -0700

    Added en.inc file

--- Koha/Plugin/EDS.pm
+++ Koha/Plugin/EDS.pm
@@ -31,10 +31,10 @@ $PluginDir = $PluginDir.'/Koha/Plugin/EDS';
 
 ################# DO NOT TOUCH - CONTROLLED BY build.py
 our $MAJOR_VERSION = "24.11";
-our $SUB_VERSION = "001";
+our $SUB_VERSION = "002";
 our $VERSION = $MAJOR_VERSION . "" . $SUB_VERSION;
 our $SHA_ADD = "https://widgets.ebscohost.com/prod/api/koha/sha/1711.json";
-our $DATE_UPDATE = '2025-04-03';
+our $DATE_UPDATE = '2025-04-04';
 ######################################################
 
 ## Here is our metadata, some keys are required, some are optional
