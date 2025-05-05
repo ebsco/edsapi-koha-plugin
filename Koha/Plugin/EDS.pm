dd946c2106a1fe8c73d2d60ef840e472476020c6
omuzzyEBSCO <omuzzy@ebsco.com>
2025-05-05 12:38:51 -0700

    Fix for EDS Autocomplete functionality

--- Koha/Plugin/EDS.pm
+++ Koha/Plugin/EDS.pm
@@ -34,7 +34,7 @@ our $MAJOR_VERSION = "24.11";
 our $SUB_VERSION = "002";
 our $VERSION = $MAJOR_VERSION . "" . $SUB_VERSION;
 our $SHA_ADD = "https://widgets.ebscohost.com/prod/api/koha/sha/1711.json";
-our $DATE_UPDATE = '2025-04-25';
+our $DATE_UPDATE = '2025-05-05';
 ######################################################
 
 ## Here is our metadata, some keys are required, some are optional
