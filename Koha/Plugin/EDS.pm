e459ed1a86f91a1314f8e53a7cb26883d9cf9ea5
omuzzyEBSCO <omuzzy@ebsco.com>
2025-09-16 10:32:33 -0700

    fix for downloadcart/sendbasket line endings

--- Koha/Plugin/EDS.pm
+++ Koha/Plugin/EDS.pm
@@ -31,10 +31,10 @@ $PluginDir = $PluginDir.'/Koha/Plugin/EDS';
 
 ################# DO NOT TOUCH - CONTROLLED BY build.py
 our $MAJOR_VERSION = "25.05";
-our $SUB_VERSION = "002";
+our $SUB_VERSION = "003";
 our $VERSION = $MAJOR_VERSION . "" . $SUB_VERSION;
 our $SHA_ADD = "https://widgets.ebscohost.com/prod/api/koha/sha/1711.json";
-our $DATE_UPDATE = '2025-09-11';
+our $DATE_UPDATE = '2025-09-16';
 ######################################################
 
 ## Here is our metadata, some keys are required, some are optional
